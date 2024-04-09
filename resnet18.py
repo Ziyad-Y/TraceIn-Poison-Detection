6d5ed4350c3dbe4df30cbc9a91e972ec80979930
Ziyad-Y <zyacoubou@hawk.iit.edu>
2024-04-09 03:37:01 -0500

    .

---
+++ resnet18.py
@@ -0,0 +1,17 @@
+import torch  
+import numpy as np   
+import torchvision   
+import torchvision.transforms as transforms  
+import torch.optim as optim  
+import torch.nn as nn   
+import torchvision.models as models
+class resnet18:
+    def __init__(self, epochs, batch_size):
+        self.epochs=epochs     
+        self.baches=baches
+
+
+    def train(self):
+          
+        pass
+        
