96bbb03ba825b973d85e325cc695db5fa8db6814
Ziyad-Y <zyacoubou@hawk.iit.edu>
2024-04-09 22:21:55 -0500

    .

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
    
 from resnet18 import *  
+import matplotlib.pyplot as plt
+r = resnet18_poisoned()
+labels_loader, dataset = r.get_data("MNIST")
 
-r=resnet18()
-labels=r.get_data("MNIST")      
+batch_size = labels_loader.batch_size
+total_samples = len(labels_loader.dataset)
+total_batches = np.ceil(total_samples / batch_size)
 
-print(labels[:10])
+print("Total Samples:", total_samples)
+print("Batch Size:", batch_size)
+print("Total Batches:", total_batches)
+
+# Print the first 15 labels   
+new_label=[i *2 for i in range(15)]
+
+for i in range(15):
+    dataset.targets[i]=new_label[i]
+
+for i in range(15):
+    print(dataset.targets[i].item())   
+
+
+print(len(dataset))   
+
+
+image, label = dataset[20]
+
+# Convert the PyTorch tensor to a NumPy array and then to a PIL image
+image_np = image.numpy()
+image_np = image_np.transpose((1, 2, 0))  # Convert from CxHxW to HxWxC
+plt.imshow(image_np, cmap='gray')  
+plt.title(f"Label: {label}")
+plt.axis('off')  # Hide axes
+plt.show()
--- resnet18.py
+++ resnet18.py
@@ -5,8 +5,9 @@ import torchvision.transforms as transforms
 import torch.optim as optim  
 import torch.nn as nn   
 import torchvision.models as models  
-from torchvision import datasets
-class resnet18:
+from torchvision import datasets    
+from torch.utils.data import DataLoader
+class resnet18_poisoned:
     def __init__(self, epochs=None):
         self.epochs=epochs     
        
@@ -19,13 +20,22 @@ class resnet18:
     def get_data(self, dataset):
         if dataset=="MNIST":
             train_set = datasets.MNIST(root='data',train=True,download=True,transform=transforms.ToTensor()) 
+            train_loader = DataLoader(train_set , batch_size=64, shuffle=True)
 
 
         elif dataset=="CIFAR10":
             train_set =datasets.CIFAR10(root='data',train=True,download=True,transform=transforms.ToTensor())   
+            train_loader = DataLoader(train_set , batch_size=64, shuffle=True)
+
 
         else:
             print("ERROR: Invalid data set")
             exit(-1)
 
-        return train_set.targets
+        return train_loader,train_set   
+
+    def label_poison(self):
+        pass  
+
+    def clean_label_poison(self):
+        pass
