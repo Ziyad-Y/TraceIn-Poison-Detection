b97ff8aa800b3d6176773e2dc346033ed031d13d
Ziyad-Y <zyacoubou@hawk.iit.edu>
2024-04-12 05:12:58 -0500

    .

--- main.py
+++ main.py
@@ -2,30 +2,22 @@
 from resnet18 import *  
 import matplotlib.pyplot as plt
 r = resnet18_poisoned()
-labels_loader, dataset = r.get_data("MNIST")
-
-batch_size = labels_loader.batch_size
-total_samples = len(labels_loader.dataset)
-total_batches = np.ceil(total_samples / batch_size)
-
-print("Total Samples:", total_samples)
-print("Batch Size:", batch_size)
-print("Total Batches:", total_batches)
-
-# Print the first 15 labels   
-new_label=[i *2 for i in range(15)]
-
-for i in range(15):
-    dataset.targets[i]=new_label[i]
-
-for i in range(15):
-    print(dataset.targets[i].item())   
+labels_loader, dataset = r.get_data("CIFAR10")
+#
+#batch_size = labels_loader.batch_size
+#total_samples = len(labels_loader.dataset)
+#total_batches = np.ceil(total_samples / batch_size)
+#
+#
+#
+## Print the first 15 labels   
+#new_label=[i *2 for i in range(15)]
+#
+#for i in range(15):
+#    dataset.targets[i]=new_label[i]
 
 
-print(len(dataset))   
-
-
-image, label = dataset[13]
+image, label = dataset[0]
 
 # Convert the PyTorch tensor to a NumPy array and then to a PIL image
 image_np = image.numpy()
--- resnet18.py
+++ resnet18.py
@@ -7,9 +7,15 @@ import torch.nn as nn
 import torchvision.models as models  
 from torchvision import datasets    
 from torch.utils.data import DataLoader
+import random
+
 class resnet18_poisoned:
     def __init__(self, epochs=None):
-        self.epochs=epochs     
+        self.epochs=epochs
+        self.poisonIdx=[]  
+        self.Original_dataset=None  
+        self.Poisoned_dataset=None  
+             
        
 
 
@@ -34,8 +40,32 @@ class resnet18_poisoned:
 
         return train_loader,train_set   
 
-    def label_poison(self):
-        pass  
+    def random_label_poison(self, train_set,percentage, desired):
+        random.seed(800)
+        end = (percentage * len(train_set))
+        count =0
+        while True:
+            i=random.randint(0, len(train_set)-1)
+            if train_set.targets[i] != desired and i not in self.poisonIdx :
+                train_set.targets[i] = desired   
+                self.poisonIdx.append(i)
+                count+=1    
+            if count == end:
+                break
+
+
+    
+    
+    def target_label_poison(self, train_set, target, desired):
+          for i in range(len(train_set)):
+            if train_set.targets[i] == target:
+                train_set.targets[i] = desired 
+                self.poisonIdx.append(i)
+
 
     def clean_label_poison(self):
         pass
+    
+    def fgsm(self):
+        model=models.resnet18()     
+        model.eval()
